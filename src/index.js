62dd04dd37599aab7eebd28a238935967569c6e6
Ｔｕｓｈａｒ　Ｔａｋ <tushartak861@gmail.com>
2023-11-06 22:37:51 +0530

    Added Basic Setup for Express-Session and Cookie-Parser

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import express from "express";
 import bodyParser from "body-parser";
+import session from "express-session";
+import cookieParser from "cookie-parser";
 
 import { connect } from "./config/db-config.js";
-import { PORT } from "./config/config.js";
+import { PORT, SECRET} from "./config/config.js";
 import v1ApiRoutes from './routes/userRoutes.js';
 
 const app = express();
 
+app.use(cookieParser());
+app.use(session({
+    secret: SECRET,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 24 * 60 * 60 * 1000
+    },
+    resave: false
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/system', v1ApiRoutes);
